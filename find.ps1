6558ee4a28625ee9408b4ebc0fcfcd80c0defd94
blameitonme1 <2741122036@qq.com>
2023-10-08 22:10:12 +0800

    修改了一些功能

--- find.ps1
+++ find.ps1
@@ -1,2 +1,13 @@
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
 $pythonScriptPath = "D:\Project\pyFind\main.py"
-python3 $pythonScriptPath 1
+$userInput = Read-Host "type 1 for open exe, 2 for set shorthand name"
+if($userInput -eq 1){
+    python3 $pythonScriptPath 1
+}
+elseif($userInput -eq 2){
+    python3 $pythonScriptPath 2
+}
+else {
+    Write-Host "invalid input"
+    # 在这里处理无效选项
+}
