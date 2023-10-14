2ba5b897a834970b860d466d2dab4ec74bee1bed
blameitonme1 <2741122036@qq.com>
2023-10-14 20:03:52 +0800

    修改了打开文件的方式和匹配，支持中文路径

--- addItem.py
+++ addItem.py
@@ -1,12 +1,14 @@
 from findItem import findItem
+import os
 def run():
-    exes = open(r"D:\Project\pyFind\allExes.txt", "r+")
+    exes = open(r"D:\Project\pyFind\allExes.txt", "r+",encoding='utf-8')
     short = input("输入一个简写名字用于查找: ")
     print("你输入的是: " + short)
     path = input("输入exe文件的路径: ")
     print("路径是: " + path)
-    cwd = input("输入exe的工作目录: ")
-    print("cwd 是 " + cwd)
+    cwd = os.path.dirname(path)
+    cwd = cwd + '"'
+    print("工作目录 是 " + cwd)
     newItem = findItem(short, path, cwd)
     if(exes.read() == ""):
         exes.write(short + " " + path + " " + cwd)
--- find.py
+++ find.py
@@ -4,16 +4,21 @@ from findItem import findItem
 import subprocess
 def run():
     short = input("输入简写名字: ")
-    exes = open(r"D:\Project\pyFind\allExes.txt", "r")
+    exes = open(r"D:\Project\pyFind\allExes.txt", "r", encoding='utf-8')
     for line in exes:
-        item = re.match(r'(\S+)\s+"([^"]+)"\s+"([^"]+)"', line)
-        if(item.group(1) == short):
-            try:
-                # print(item.group(1) + item.group(2))
-                subprocess.Popen(item.group(2), shell=True, cwd=item.group(3))
-                print("打开文件成功\n")
-            except FileNotFoundError:
-                print("没有找到该文件, 可能要更新路径\n")
-            except Exception as e:
-                print("发生了错误: " + str(e) + '\n')
+        item = re.match(r'(\S+)\s+"([^"]+)"\s+"([^"]+)"', line, re.UNICODE)
+        try:
+            if(item.group(1) == short):
+                try:
+                    # print(item.group(1) + item.group(2))
+                    subprocess.Popen(item.group(2), shell=True, cwd=item.group(3))
+                    print("打开文件成功\n")
+                except FileNotFoundError:
+                    print("没有找到该文件, 可能要更新路径\n")
+                except Exception as e:
+                    print("发生了错误: " + str(e) + '\n')
+                break
+        except AttributeError as e:
+            print(f"{e}, invalid format")
+
     print("进程结束\n")
